cff484e55186be58e032ee96e2d20046cbeac8ae
akhilesharora <akhildawra@gmail.com>
2017-10-04 20:31:27 +0530

    exception handled for file operations

--- game.rb
+++ game.rb
@@ -1,17 +1,27 @@
 #!/usr/bin/env ruby
 load 'pyscho.rb'
 
-if ARGV.size < 2
-  where = ARGV.size == 0 ? "Input" : "Output"
-  raise ArgumentError.new("#{where} no valid file found.")
-end
+begin
+	if ARGV.size < 2
+    where = ARGV.size == 0 ? "Input" : "Output"
+    raise ArgumentError
 
-input_file = File.open(ARGV[0])
-output_file = File.open(ARGV[1], "w")
+  end
 
-input_file.read.split("\n").each do |line|
-  line = line.split(" ")
-  game = Pyscho.new(line[0, 5], line[5, 5])
-  hand, deck, best_hand = game.cards_in_hand.join(' '), game.cards_in_deck.join(' '), game.best_hand
-  output_file.write "Hand: #{hand} Deck: #{deck} Best hand: #{best_hand}\n"
+  input_file = File.open(ARGV[0])
+  output_file = File.open(ARGV[1], "w")
+
+  input_file.read.split("\n").each do |line|
+    line = line.split(" ")
+    game = Pyscho.new(line[0, 5], line[5, 5])
+    hand, deck, best_hand = game.cards_in_hand.join(' '), game.cards_in_deck.join(' '), game.best_hand
+    output_file.write "Hand: #{hand} Deck: #{deck} Best hand: #{best_hand}\n"
+  end
+
+  rescue ArgumentError
+    puts "#{where} file not found."
+  rescue TypeError
+    puts 'Issue with File Handling'
+  rescue NoMethodError
+    puts 'File not opened'
 end
