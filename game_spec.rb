131961bd26904600cf983753765c39774bcda66b
akhilesharora <akhildawra@gmail.com>
2017-10-04 20:00:47 +0530

    adding spec file

---
+++ game_spec.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+load 'pyscho.rb'
+
+describe Pyscho do
+  
+end
--- pyscho.rb
+++ pyscho.rb
@@ -52,15 +52,7 @@ class Pyscho
       @cards_in_hand.combination(card_number_in_hand).each do |combination|
         combination.each_with_index {|buf,buf_index| @simulatedHands[@simulatedHands.index(buf)] = @cards_in_deck[buf_index]}
         @simulatedHands.sort! {|key,val| FACES.index(key[0]) <=> FACES.index(val[0]);}
-        probable_best_hand = ('straight-flush' if is_straight? && is_flush?) ||
-                 ('four-of-a-kind' if kinds? [4, 1]) ||
-                 ('full-house' if kinds? [3, 2]) ||
-                 ('flush' if is_flush?) ||
-                 ('straight' if is_straight?) ||
-                 ('three-of-a-kind' if kinds? [3, 1, 1]) ||
-                 ('two-pairs' if kinds? [2, 2, 1]) ||
-                 ('one-pair' if kinds? [2, 1, 1, 1]) ||
-                 'highest-card'
+        probable_best_hand = ('straight-flush' if is_straight? && is_flush?) || ('four-of-a-kind' if kinds? [4, 1]) || ('full-house' if kinds? [3, 2]) || ('flush' if is_flush?) || ('straight' if is_straight?) || ('three-of-a-kind' if kinds? [3, 1, 1]) || ('two-pairs' if kinds? [2, 2, 1]) || ('one-pair' if kinds? [2, 1, 1, 1]) || 'highest-card'
         best_hand = probable_best_hand if COMBOS.index(probable_best_hand) > COMBOS.index(best_hand)
         return best_hand if best_hand.eql?('straight-flush')
         @simulatedHands = @cards_in_hand.dup
